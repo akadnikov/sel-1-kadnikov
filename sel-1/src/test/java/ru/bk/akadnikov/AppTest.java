3027593c7657af17e55862b3d85ede4af91aaa65
akadnikov <kadnikov.ant@gmail.com>
2016-12-03 11:39:28 +0400

    initial version

---
+++ sel-1/src/test/java/ru/bk/akadnikov/AppTest.java
@@ -0,0 +1,33 @@
+package ru.bk.akadnikov;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class AppTest {
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @Before
+    public void start() {
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 10);
+    }
+
+    @Test
+    public void myTest() {
+        driver.get("http://www.google.com/");
+    }
+
+    @After
+    public void stop() {
+        driver.quit();
+        driver = null;
+    }
+
+}
