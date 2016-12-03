ca5e4c3af925c24406b861c1c26f3907449aa447
akadnikov <kadnikov.ant@gmail.com>
2016-12-03 13:47:51 +0400

    Задание 3

--- sel-1/src/test/java/ru/bk/akadnikov/AppTest.java
+++ sel-1/src/test/java/ru/bk/akadnikov/AppTest.java
@@ -3,10 +3,13 @@ package ru.bk.akadnikov;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
 
 public class AppTest {
 
@@ -17,11 +20,15 @@ public class AppTest {
     public void start() {
         driver = new ChromeDriver();
         wait = new WebDriverWait(driver, 10);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
 
     @Test
     public void myTest() {
-        driver.get("http://www.google.com/");
+        driver.get("http://localhost/litecart/admin");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("login")).click();
     }
 
     @After
